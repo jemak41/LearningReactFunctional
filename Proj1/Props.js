908276528f08d71901f45578aebeddb589efe953
Jemak Posadas <justinemarkposadas@Jemaks-iMac.local>
2019-05-04 14:54:07 +0800

    Notes

--- Proj1/Props.js
+++ Proj1/Props.js
@@ -30,3 +30,17 @@ export default class LotsOfGreetings extends Component {
     );
   }
 }
+
+var createScream = function(logger) { 
+  return function(message) {
+    logger(message.toUpperCase() + "!!!")
+  }
+}
+
+const scream = createScream(message => console.log(message))
+scream('functions can be returned from other functions')
+scream('createScream returns a function')
+scream('scream invokes that returned function')
+// FUNCTIONS CAN BE RETURNED FROM OTHER FUNCTIONS!!!
+// CREATESCREAM RETURNS A FUNCTION!!!
+// SCREAM INVOKES THAT RETURNED FUNCTION!!!
