edd1d0522d747c4a52995c693209bbc98ebd850f
Jemak <jmacposadas@gmail.com>
2019-05-06 19:20:15 +0800

    Correcting

--- FreeCodeCamp/project/src/App.js
+++ FreeCodeCamp/project/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Footer from "./Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div>
+            <h1>JEMAK GWAPO</h1>
+            <p>blah blah</p>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+            <Footer/>
+        </div>
+    )
 }
 
-export default App;
+export default App
